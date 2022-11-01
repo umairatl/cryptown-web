837107bd45cffea08615f0a688ab070740c0f4bc
umairatl <umairatul@besquare.com.my>
2022-11-01 13:47:07 +0800

    add exchange list

--- src/views/forumPage/forum.js
+++ src/views/forumPage/forum.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 const ForumPage = () => {
+    const [crypto, setCrypto] = useState(null)
+    useEffect(() => {
+        const fetchCrypto = async () => {
+            const response = await axios('https://localhost:5000/api/crypto/cryptoList')
+            const json = await response.data
+    
+            if (response.status === 200) {
+                setCrypto(json)
+            }
+        }
+        fetchCrypto()
+    
+    
+      }, [])
+
     return (  
         <div className="forum">
             <h1>FORUM PAGE</h1>
+            <h2>{crypto && (<div>{crypto.mssg}</div>)}</h2>
+            {/* {crypto && crypto.cryptoList.map(res => <div>{res.name}</div>)} */}
         </div>
     );
 }
