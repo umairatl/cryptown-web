0acf3ce82f304a77a5ee00211c1f8d9c87de2b07
adi <aditya.bardhan@besquare.com.my>
2022-11-02 01:53:21 +0000

    port

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [crypto, setCrypto] = useState(null)
+  const [crypto, setCrypto] = useState(null);
 
   useEffect(() => {
     const fetchCrypto = async () => {
-        const response = await axios('http://localhost:5000/cryptown/api')
-        const json = await response.data
+      const response = await axios("http://localhost:6000/cryptown/api");
+      const json = await response.data;
 
-        if (response.status === 200) {
-            setCrypto(json)
-        }
-    }
-    fetchCrypto()
-    
-    
-  }, [])
+      if (response.status === 200) {
+        setCrypto(json);
+      }
+    };
+    fetchCrypto();
+  }, []);
 
   return (
     <div>
-      <h2>{crypto && (<div>{crypto.mssg}</div>)}</h2>
+      <h2> hello {crypto && <div>{crypto.mssg}</div>}</h2>
     </div>
   );
 }
