91b7a9ec3e9a89a739cbb87fc348edf1add4d4ba
umairatl <umairatul@besquare.com.my>
2022-11-09 14:02:00 +0800

    fix navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  Navbar from "./components/navbar/navbar";
 import Signup from "./views/signupPage/signup";
-// import Login from './views/login/login';
 import FavPage from "./views/favouritePage/fav";
 import CryptoList from "./views/cryptoList/coin";
 import AppList from "./views/appList/appList";
@@ -12,30 +11,41 @@ import NewsPage from "./views/news/news";
 import Profile from "./views/profilePage/profile";
 import CoinDetail from "./views/coinDetail/coinDetail";
 import Footer from "./components/footer/footer";
+import { Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+import NotFound from './components/notFound/notFound';
 
 function App() {
-  // const [token, setToken] =  useState();
-
-  // if(!token){
-  //   return <Login setToken={setToken} />
-  // }
+  const { user } = useAuthContext()
 
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/fav" element={<FavPage />}></Route>
-          <Route path="/" element={<CryptoList />}></Route>
-          <Route path="/coinDetail/:id" element={<CoinDetail />}></Route>
-          <Route path="/forum" element={<Forum />}></Route>
-          <Route path="/appList" element={<AppList />}></Route>
-          <Route path="/news" element={<NewsPage />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
+      <Routes>
+            <Route path="/fav" element = { <FavPage />}>
+            </Route>
+            <Route path="/" element = { <CryptoList />}>
+            </Route> 
+            <Route path="/coinDetail/:id" element = { <CoinDetail />}>
+            </Route> 
+            <Route path="/forum" element = { <Forum />}>
+            </Route>
+            <Route path="/forum" element = { user ? <Forum /> : <Navigate to="/login" />}>
+            </Route>
+            <Route path="/appList" element = { <AppList />}>
+            </Route>
+          <Route path="/news" element={<NewsPage />}>
+          </Route>
+          <Route path="/profile" element={<Profile />}>
+          </Route>
+          <Route path="/signup" element={<Signup />}>
+            
+          </Route>
+          <Route path="/watchlist" element = { user ? <FavPage /> : <Navigate to="/login" />}>
+            </Route>
+            <Route path='*' element={<NotFound />}/>
         </Routes>
       </div>
-       <Footer /> 
     </BrowserRouter>
   );
 }
