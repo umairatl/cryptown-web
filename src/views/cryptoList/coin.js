f17ac7c654f89154a5b5346196f4956f2d464dcf
adi <aditya.bardhan@besquare.com.my>
2022-11-09 06:36:24 +0000

    pagination

--- src/views/cryptoList/coin.js
+++ src/views/cryptoList/coin.js
@@ -10,19 +10,23 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { useNavigate } from "react-router-dom";
-
+import { Carousel } from 'react-responsive-carousel';
+import { Pagination } from '@mui/material';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import ReactDOM from 'react-dom';
 
 const Coin = ({}) => {
   const [crypto, setCrypto] = useState(null);
   const [search, setSearch] = useState("");
   const [tren, setTren] = useState(null);
-    const navigation = useNavigate();
+  const [page, setPage] = useState(1);
+  const navigation = useNavigate();
   let arr = [];
 
   useEffect(() => {
     const fetchCrypto = async () => {
       const response = await axios(
-        "https://localhost:5000/api/crypto/cryptoList"
+        "https://localhost:8443/api/crypto/cryptoList"
       );
       const json = await response.data;
 
@@ -37,7 +41,7 @@ const Coin = ({}) => {
     useEffect(() => {
     const fetchCryptoTren = async () => {
       const response = await axios(
-        "https://localhost:5000/api/crypto/cryptoTrending"
+        "https://localhost:8443/api/crypto/cryptoTrending"
       );
       const json = await response.data;
 
@@ -48,24 +52,35 @@ const Coin = ({}) => {
     fetchCryptoTren();
   }, []);
 
-  const cryptoFilter = (e) => {
+  const cryptoFilter = () => {
     return crypto?.cryptoList.filter(
       (f) =>
-        f.name.toLowerCase().includes(e) ||
-        f.symbol.toLowerCase().includes(e) ||
-        f.cryptoId.toLowerCase().includes(e)
+        f.name.toLowerCase().includes(search) ||
+        f.symbol.toLowerCase().includes(search) ||
+        f.cryptoId.toLowerCase().includes(search)
     );
   };
+
 
   return (
     <div>
       {tren &&
         tren.cryptoTrending.map((res) => (
           <div>
-            <img src={res.image} />
-            <p className="legend">{res.name} </p>
+
+               <Carousel>
+                <div>
+                    <img src={res.image}  />
+                    <p className="legend">{res.name}</p>
+                    <p className="legend">{res.symbol}</p>
+                    
+                </div>
+               </Carousel>  
+            {/* <img src={res.image} onDragStart={handleDragStart} role="presentation" />, */}
+            {/* <img src={res.image} /> */}
+            {/* <p className="legend">{res.name} </p>
             <p className="legend">{res.symbol} </p>
-            <p className="legend">{res.cryptoId} </p>
+            <p className="legend">{res.cryptoId} </p> */}
             {/* <AliceCarousel mouseTracking items={items} /> */}
           </div>
         ))}
@@ -95,31 +110,52 @@ const Coin = ({}) => {
               <TableCell>Market Cap</TableCell>
             </TableRow>
            </TableHead>
+           
+
+
         <TableBody>
         {crypto &&
-            cryptoFilter(search).map((data) => (
-            <TableRow key={data.name} style={{cursor:'pointer'}} 
-            onClick={() => {
-              navigation(`/coinDetail/${data.cryptoId}`);
-            }}
-            >
-                <TableCell>{data.market_cap_rank}</TableCell>
-                   <TableCell>
-                    <img src={data.image} width='40px'></img>
-                   </TableCell>
-                <TableCell>
-                    {data.name}
-                    </TableCell>
-                <TableCell>${data.current_price}</TableCell>
-                <TableCell>{data.market_cap} / 10</TableCell>
-              {/* </Link> */}
-
-            </TableRow>
-          ))}
+            cryptoFilter()
+            .slice((page - 1) * 10, (page - 1) * 10 + 10)
+            .map((data) => {
+              // const marketCap = data.market_cap_rank > 0;
+              return (
+                <TableRow key={data.name} style={{cursor:'pointer'}} 
+                onClick={() => {
+                  navigation(`/coinDetail/${data.cryptoId}`);
+                }}
+                >
+                    <TableCell>{data.market_cap_rank}</TableCell>
+                      <TableCell>
+                        <img src={data.image} width='40px'></img>
+                      </TableCell>
+                    <TableCell>
+                        {data.name}
+                        </TableCell>
+                    <TableCell>${data.current_price}</TableCell>
+                    <TableCell>{data.market_cap} </TableCell>
+                  {/* </Link> */}
+                </TableRow>
+                )
+            }
+            )}
         </TableBody>
       </Table>
     </TableContainer>
 
+ <Pagination
+          count={(cryptoFilter()?.length / 10).toFixed(0)}
+          style={{
+            padding: 20,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          onChange={(_, value) => {
+            setPage(value);
+            window.scroll(0, 450);
+          }}
+        />
         {/* <h2>
           {crypto &&
             cryptoFilter(search).map((data) => (
