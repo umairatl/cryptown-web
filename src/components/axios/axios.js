bfe05b6abe578388b4753f926cf92abf800222b9
umairatl <umairatul@besquare.com.my>
2022-11-11 10:33:50 +0800

    ui login/signup

--- src/components/axios/axios.js
+++ src/components/axios/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const instance = axios.create({baseURL: 'https://localhost:5000/'});
+const instance = axios.create({baseURL: 'http://localhost:5000/'});
 
 export default instance
